d41fbe4630de4e9a66e9c78ef46e2ecbe2632b0e
Furkan Emin <femincanx@gmail.com>
2022-12-22 09:42:34 +0300

    fix: cannot detect file existence and path problem

--- url.controller.js
+++ url.controller.js
@@ -1,14 +1,18 @@
 const fs = require('node:fs');
+const path = require('node:path');
 
-const FILE_PATH = `${process.cwd()}/urls.json`;
-const isFileExist = fs.existsSync(FILE_PATH);
+const FILE_PATH = path.resolve('./urls.json');
+
+function isFileExist() {
+  return fs.existsSync(FILE_PATH);
+}
 
 function getUrls() {
-  return isFileExist ? JSON.parse(fs.readFileSync(FILE_PATH, 'utf-8')) : null;
+  return isFileExist() ? JSON.parse(fs.readFileSync(FILE_PATH, 'utf-8')) : null;
 }
 
 function saveUrl(url, id) {
-  if (isFileExist) {
+  if (isFileExist()) {
     const urls = JSON.parse(fs.readFileSync(FILE_PATH, 'utf-8'));
 
     fs.writeFileSync(FILE_PATH, JSON.stringify({ ...urls, [id]: url }));

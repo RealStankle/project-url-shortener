0ac1a7e810b92a4d6061d46af21c123ca8e164c1
Furkan Emin <femincanx@gmail.com>
2022-12-22 09:27:01 +0300

    feat: add project code

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const validator = require('validator');
+const { randomUUID } = require('node:crypto');
+const { getUrls, saveUrl } = require('./url.controller');
+
+require('dotenv').config();
+
 const app = express();
 
-// Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (_, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url;
+  const isValidURL = validator.isURL(url, {
+    require_protocol: true,
+  });
+
+  if (isValidURL) {
+    const urlID = randomUUID().slice(0, 8);
+
+    saveUrl(url, urlID);
+
+    res.json({
+      original_url: url,
+      short_url: urlID,
+    });
+  } else {
+    res.json({ error: 'invalid url' });
+  }
 });
 
-app.listen(port, function() {
+app.get('/api/shorturl/:urlID', (req, res) => {
+  const urlID = req.params.urlID;
+  const urls = getUrls();
+
+  if (urls && Object.keys(urls).includes(urlID)) {
+    res.redirect(urls[urlID]);
+  } else {
+    res.redirect('/');
+  }
+});
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
